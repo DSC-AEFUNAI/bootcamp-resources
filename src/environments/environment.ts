12469826990e1e7c8d0c397a27f9ad478b1b33b1
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-11-08 00:25:59 +0100

    install firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAmHM1k3hNg6DAttz2-uhf0IXA-eOIzU3I",
+    authDomain: "dsc-aefunai-bootcamp.firebaseapp.com",
+    databaseURL: "https://dsc-aefunai-bootcamp.firebaseio.com",
+    projectId: "dsc-aefunai-bootcamp",
+    storageBucket: "dsc-aefunai-bootcamp.appspot.com",
+    messagingSenderId: "803215495386",
+    appId: "1:803215495386:web:d6f4ab8bedd7f8d55495e3"
+  }
 };
 
 /*
